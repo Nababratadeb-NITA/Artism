e3d4ec5892efe547a0fc6ad4e0cad2499d73d840
Nababrata Deb <116813147+Nababratadeb-NITA@users.noreply.github.com>
2022-12-28 13:57:26 +0530

    head

--- components/Header.js
+++ components/Header.js
@@ -50,7 +50,7 @@ function Header() {
             </h1>
           </>
         ) : (
-          <HiUserCircle className="headerIcon" onClick={() => signIn()} />
+          <HiUserCircle className="h-10" onClick={() => signIn()} />
         )}
         {/* //cart */}
         <div
