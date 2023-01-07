f517f9f4e16d5bfbed1c8ce31b9ac7a4f0d6fa27
unknown <nababrata13@gmail.com>
2023-01-07 17:03:38 +0530

    change 18

--- components/Product.js
+++ components/Product.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 import { urlFor } from "../lib/client";
 
-const Product = ({ product: { image, name, price, _id } }) => {
+const Product = ({ product: { image, name, price, _id, postedBy } }) => {
   return (
     <div>
       <Link href={`/product/${_id}`}>
         <div className="flex flex-col m-5 bg-white z-30 p-10 rounded-md product-card">
           <img
-            className="product-image h-64 bg-contain"
+            className="product-image h-64 object-contain"
             src={urlFor(image)}
             alt="img"
           />
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,14 +1,10 @@
-import axios from "axios";
-import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
-import Product from "../../components/Product";
+
 import { useStateContext } from "../../context/StateContext";
 import { client, urlFor } from "../../lib/client";
 
-function ProductDetails({ products, product }) {
-  const [post, setPost] = useState(product);
-
+function ProductDetails({ product }) {
   const { image, name, details, price, postedBy } = product;
   const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
@@ -18,64 +14,54 @@ function ProductDetails({ products, product }) {
     setShowCart(true);
   };
 
-  console.log(product);
-
   return (
     <>
-      {post && (
-        <div>
-          <div className="product-detail-container flex flex-col">
-            <div>
-              <div className="image-container">
-                <img src={urlFor(image)} className="product-detail-image" />
-              </div>
-              
+      <div>
+        <div className="product-detail-container flex flex-col">
+          <div>
+            <div className="image-container">
+              <img src={urlFor(image)} className="product-detail-image" />
             </div>
-
-            <div className="product-detail-desc">
-              <h1 className="space-y-2 text-xl font-bold md:text-2xl">
-                {name}
-              </h1>
-              <p className="font-base"> by {postedBy}</p>
-              <p className="price">Rs. {price}</p>
-              <div className="quantity">
-                <h3>Quantity:</h3>
-                <p className="quantity-desc flex items-center justify-center">
-                  <span
-                    className="minus h-12 justify-center items-center flex"
-                    onClick={decQty}>
-                    <AiOutlineMinus />
-                  </span>
-                  <span className="num h-12 justify-center items-center flex">
-                    {qty}
-                  </span>
-                  <span
-                    className="plus h-12 justify-center items-center flex"
-                    onClick={incQty}>
-                    <AiOutlinePlus />
-                  </span>
-                </p>
-              </div>
-              <div className="buttons">
-                <button
-                  type="button"
-                  className="add-to-cart"
-                  onClick={() => onAdd(product, qty)}>
-                  Add to Cart
-                </button>
-                <button
-                  type="button"
-                  className="buy-now"
-                  onClick={handleBuyNow}>
-                  Buy Now
-                </button>
-              </div>
-            </div>
-            <h2>Details: </h2>
-              <p>{details}</p>
           </div>
+
+          <div className="product-detail-desc">
+            <h1 className="space-y-2 text-xl font-bold md:text-2xl">{name}</h1>
+            <p className="font-base"> CREATED BY {postedBy}</p>
+            <p className="price">Rs. {price}</p>
+            <div className="quantity">
+              <h3>Quantity:</h3>
+              <p className="quantity-desc flex items-center justify-center">
+                <span
+                  className="minus h-12 justify-center items-center flex"
+                  onClick={decQty}>
+                  <AiOutlineMinus />
+                </span>
+                <span className="num h-12 justify-center items-center flex">
+                  {qty}
+                </span>
+                <span
+                  className="plus h-12 justify-center items-center flex"
+                  onClick={incQty}>
+                  <AiOutlinePlus />
+                </span>
+              </p>
+            </div>
+            <div className="buttons">
+              <button
+                type="button"
+                className="add-to-cart"
+                onClick={() => onAdd(product, qty)}>
+                Add to Cart
+              </button>
+              <button type="button" className="buy-now" onClick={handleBuyNow}>
+                Buy Now
+              </button>
+            </div>
+          </div>
+          <h2>Details: </h2>
+          <p>{details}</p>
         </div>
-      )}
+      </div>
     </>
   );
 }
