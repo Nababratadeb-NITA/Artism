ea889410f036e89256a2273a08d3fa7540209620
unknown <nababrata13@gmail.com>
2023-01-09 21:53:55 +0530

    change 20

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,34 +1,78 @@
 import { Tab } from "@headlessui/react";
 
-import React from "react";
+import React, { useState } from "react";
 
 const Category = ({ category, products }) => {
-  const getUniqueData = (data, property) => {
-    let newVal = data.map((curElem) => {
-      return curElem[property];
-    });
-    return (newVal = ["All", ...new Set(newVal)]);
-  };
+  const [items, setItems] = useState(products);
 
-  const categoryData = getUniqueData(products, "category");
+  const filter = (cetItem) => {
+    const updatedItems = products.filter((elem) => {
+      return elem.category === cetItem;
+    });
+    setItems(updatedItems);
+    console.log(updatedItems);
+  };
+  console.log(products);
 
   return (
     <>
       <Tab.Group>
         <Tab.List className="flex justify-center">
-          {category.map((fn) => (
-            <Tab
-              key={fn._id}
-              className={({ selected }) =>
-                `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
-                  selected
-                    ? "borderGradient bg-[#324d67] text-white"
-                    : "border-b-2 border-[#324d67] text-[#747474]"
-                }`
-              }>
-              {fn.title}
-            </Tab>
-          ))}
+          <Tab
+            onClick={() => setItems(products)}
+            className={({ selected }) =>
+              `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                selected
+                  ? "borderGradient bg-[#324d67] text-white"
+                  : "border-b-2 border-[#324d67] text-[#747474]"
+              }`
+            }>
+            ALL
+          </Tab>
+          <Tab
+            onClick={() => filter("3D")}
+            className={({ selected }) =>
+              `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                selected
+                  ? "borderGradient bg-[#324d67] text-white"
+                  : "border-b-2 border-[#324d67] text-[#747474]"
+              }`
+            }>
+            3D
+          </Tab>
+          <Tab
+            onClick={() => filter("2D")}
+            className={({ selected }) =>
+              `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                selected
+                  ? "borderGradient bg-[#324d67] text-white"
+                  : "border-b-2 border-[#324d67] text-[#747474]"
+              }`
+            }>
+            2D
+          </Tab>
+          <Tab
+            onClick={() => filter("FPS")}
+            className={({ selected }) =>
+              `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                selected
+                  ? "borderGradient bg-[#324d67] text-white"
+                  : "border-b-2 border-[#324d67] text-[#747474]"
+              }`
+            }>
+            FPS
+          </Tab>
+          <Tab
+            onClick={() => filter("RPG")}
+            className={({ selected }) =>
+              `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                selected
+                  ? "borderGradient bg-[#324d67] text-white"
+                  : "border-b-2 border-[#324d67] text-[#747474]"
+              }`
+            }>
+            RPG
+          </Tab>
         </Tab.List>
       </Tab.Group>
     </>
