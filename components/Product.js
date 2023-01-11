86306d1143e87ac41cb371df5224249255dce6cd
unknown <nababrata13@gmail.com>
2023-01-11 15:51:54 +0530

    change 20

--- components/Product.js
+++ components/Product.js
@@ -7,14 +7,22 @@ const Product = ({ product: { image, name, price, _id, postedBy } }) => {
   return (
     <div>
       <Link href={`/product/${_id}`}>
-        <div className="flex flex-col m-5 bg-white z-30 p-10 rounded-md product-card">
+        <div className="flex flex-col m-5 bg-white z-30 p-5 rounded-md product-card">
+          <div className="mb-3 flex flex-col justify-start">
+            <p>Creator</p>
+            <h2 className="font-semibold">{postedBy}</h2>
+          </div>
           <img
             className="product-image h-64 object-contain"
             src={urlFor(image)}
             alt="img"
           />
           <p className="product-name italic">{name}</p>
-          <p className="product-price my-3">Rs. {price}</p>
+          <div className="h-[1px] bg-gray-600 w-full mt-2"></div>
+          <div className="flex justify-between mt-2">
+            <p className="product-price my-3">Rs. {price}</p>
+            <div className="p-2 bg-yellow-500 rounded-lg">Desc</div>
+          </div>
         </div>
       </Link>
     </div>
