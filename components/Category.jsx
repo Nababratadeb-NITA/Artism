0cc3485485ed45ae9dae4bd3237d639090595e67
Nababrata Deb <116813147+Nababratadeb-NITA@users.noreply.github.com>
2023-01-12 01:13:06 +0530

    update

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,64 +1,96 @@
-import { Tab } from "@headlessui/react";
+import { Tab } from '@headlessui/react';
 
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-const Category = ({ category, products }) => {
+const Category = ({ category, products, filterProducts, resetProducts }) => {
   const [items, setItems] = useState(products);
 
-  const filter = (cetItem) => {};
+  const filter = (cetItem) => {
+    const updatedItems = products.filter((elem) => {
+      return elem.category === cetItem;
+    });
+    setItems(updatedItems);
+    filterProducts(updatedItems);
+    console.log(updatedItems);
+  };
+  console.log(products);
+
   return (
     <>
       <Tab.Group>
         <Tab.List className="flex justify-center">
           <Tab
+            onClick={() => {
+              resetProducts();
+            }}
             className={({ selected }) =>
               `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
                 selected
-                  ? "borderGradient bg-[#324d67] text-white"
-                  : "border-b-2 border-[#324d67] text-[#747474]"
+                  ? 'borderGradient bg-[#324d67] text-white'
+                  : 'border-b-2 border-[#324d67] text-[#747474]'
               }`
-            }>
+            }
+          >
             ALL
           </Tab>
           <Tab
-            onClick={() => filter("3D")}
+            onClick={() => {
+              resetProducts();
+              filter('3D');
+            }}
             className={({ selected }) =>
               `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
                 selected
-                  ? "borderGradient bg-[#324d67] text-white"
-                  : "border-b-2 border-[#324d67] text-[#747474]"
+                  ? 'borderGradient bg-[#324d67] text-white'
+                  : 'border-b-2 border-[#324d67] text-[#747474]'
               }`
-            }>
+            }
+          >
             3D
           </Tab>
           <Tab
+            onClick={() => {
+              resetProducts();
+              filter('2D');
+            }}
             className={({ selected }) =>
               `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
                 selected
-                  ? "borderGradient bg-[#324d67] text-white"
-                  : "border-b-2 border-[#324d67] text-[#747474]"
+                  ? 'borderGradient bg-[#324d67] text-white'
+                  : 'border-b-2 border-[#324d67] text-[#747474]'
               }`
-            }>
+            }
+          >
             2D
           </Tab>
           <Tab
+            onClick={() => {
+              resetProducts();
+              filter('FPS');
+            }}
             className={({ selected }) =>
               `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
                 selected
-                  ? "borderGradient bg-[#324d67] text-white"
-                  : "border-b-2 border-[#324d67] text-[#747474]"
+                  ? 'borderGradient bg-[#324d67] text-white'
+                  : 'border-b-2 border-[#324d67] text-[#747474]'
               }`
-            }>
+            }
+          >
             FPS
           </Tab>
           <Tab
+            onClick={() => {
+              resetProducts();
+              filter('RPG');
+            }}
             className={({ selected }) =>
               `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
                 selected
-                  ? "borderGradient bg-[#324d67] text-white"
-                  : "border-b-2 border-[#324d67] text-[#747474]"
+                  ? 'borderGradient bg-[#324d67] text-white'
+                  : 'border-b-2 border-[#324d67] text-[#747474]'
               }`
-            }>
+            }
+          >
             RPG
           </Tab>
         </Tab.List>
