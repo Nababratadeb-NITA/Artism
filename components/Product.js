e038761a06ea82b6afd5ab3c0b60f0556bba42a9
unknown <nababrata13@gmail.com>
2023-02-04 14:11:52 +0530

    fix issue

--- components/Product.js
+++ components/Product.js
@@ -14,45 +14,21 @@ const Product = ({ product }) => {
   return (
     <div className="flex flex-col col-span-12 bg-white rounded-lg cursor-pointer md:col-span-6 xl:col-span-3 h-fit product-card">
       <Link href={`/product/${_id}`}>
-
         <div className="flex flex-col m-5 bg-white z-30 p-5 rounded-md product-card">
-          <div className="mb-3 flex justify-start">
-            {userImg ? (
-              <img
-                width={45}
-                height={10}
-                src={userImg}
-                className="w-8 h-8 rounded-full"
-                alt={name}
-              />
-            ) : (
-              <div className="w-8 h-8 font-semibold bg-black rounded-full"></div>
-            )}
-            <div>
-              <p>Creator</p>
-              <h2 className="font-semibold">{postedBy}</h2>
-            </div>
+          <div className="w-full max-h-1/2">
+            <img
+              src={urlFor(image)}
+              className="object-cover w-full rounded-tl-lg rounded-tr-lg aspect-square"
+            />
           </div>
-          <img className="product-image h-64" src={urlFor(image)} alt="img" />
-          <p className="product-name italic">{name.substr(0, 15)}</p>
-          <div className="h-[1px] bg-gray-600 w-full mt-2"></div>
-          <div className="flex justify-between mt-2">
-            <p className="product-price my-3">Rs. {price}</p>
-            <div className="p-2 bg-yellow-500 rounded-lg">Desc</div>
-          </div>
-
-        <div className="w-full max-h-1/2">
-          <img
-            src={urlFor(image)}
-            className="object-cover w-full rounded-tl-lg rounded-tr-lg aspect-square"
-          />
-
         </div>
       </Link>
 
       <Link href={`/product/${_id}`}>
         <div className="flex items-center justify-between px-3 mt-4">
-          <h3 className="text-xl font-semibold text-gray-700">{name.substr(0, 20)}</h3>
+          <h3 className="text-xl font-semibold text-gray-700">
+            {name.substr(0, 20)}
+          </h3>
           <span className="text-gray-700">&#8377;{price}</span>
         </div>
       </Link>
@@ -75,8 +51,7 @@ const Product = ({ product }) => {
 
       <button
         className="flex items-center justify-center gap-3 py-3 mx-4 my-4 text-white bg-orange-500 rounded-lg hover:bg-orange-600"
-        onClick={addToCart}
-      >
+        onClick={addToCart}>
         <MdAddShoppingCart />
         <span className="leading-none">Add to Cart</span>
       </button>
